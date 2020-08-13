0a49de1d054420c6d10a3aa4c540a845bdef3111
wd30gsrc <gd28.c.tateno@gmail.com>
2020-08-13 10:28:21 +0900

    整形

--- src/Article02.jsx
+++ src/Article02.jsx
@@ -5,9 +5,10 @@ const Article02 = (props) => {
     <div>
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態:</label>
-      <input type="checkbox"
-        checked={props.isPublished}
+      <input
         id="check"
+        type="checkbox"
+        checked={props.isPublished}
         onClick={() => props.toggle()}>
       </input>
     </div>
